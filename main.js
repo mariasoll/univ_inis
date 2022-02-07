f96ad1696ad3ef8865ef16b19af277b04ba7683a
mariasoll <mariasoltan877@gmail.com>
2022-02-07 16:42:26 +0300

    Add files

---
+++ main.js
@@ -0,0 +1,16 @@
+// NOTE: The variable "shirts" is defined in the shirts.js file as the full list of shirt offerings
+//       You can access this variable here, and should use this variable here to build your webpages
+
+let initProducts = () => {
+    // To see the shirts object, run:
+    // console.log(shirts);
+
+    // Your Code Here
+};
+
+let initDetails = () => {
+    // To see the shirts object, run:
+    // console.log(shirts);
+
+    // Your Code Here
+};
